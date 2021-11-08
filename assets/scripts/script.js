88f1ae5f934d778316f8fab0a08a7c016f604253
NatanGtZ <natan.gabriel@uneosc.edu.br>
2021-11-07 23:01:51 -0300

    header not responsive yet

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,11 +7,11 @@
     window.addEventListener('scroll', function() {
         if(window.scrollY > 0 ){
             menu.classList.add('MenuFixo');
-            imagem.setAttribute('src', 'assets/images/Logobranco.png');
+            imagem.setAttribute('src', 'assets/images/Logobranco.svg');
         }
         else {
             menu.classList.remove('MenuFixo');
-            imagem.setAttribute('src', 'assets/images/logopreto.png');
+            imagem.setAttribute('src', 'assets/images/logopreto.svg');
         }
     });
 })();
